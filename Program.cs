b8e4687620d2e83083b82d64c9bbcfe26437fd04
klemawa <116188369+klemawa@users.noreply.github.com>
2023-10-17 19:01:03 +0200

    pktrandom

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,42 +11,58 @@ namespace OdliczanieOdleglosciMiedzyPkt
     {
         static void Main(string[] args)
         {
-
+            randomPkt randomPkt = new randomPkt();
+            double n = 10;
+            randomPkt.RandomGeneration(n);
+            randomPkt.PktPoczatekIKoniec(0 ,0);
         }
 
         public class randomPkt
         {
             double k;
-            double n = 10;
-            double szerokoscGeo;
-            double dlugoscGeo;
             List<double> listaWygenerowanychPkt = new List<double>();
 
-            public RandomGeneration(double n,double k) 
+            public double RandomGeneration(double n) 
             {
-                Random randomk = new Random();
-                k = randomk.NextDouble();
-                Console.WriteLine($"Liczba przejść przez punkty jest równa: {k}");
-
+                Random randomk = new Random(); //losowanie liczby przejść k
+                k = randomk.NextDouble()*(n-2)+1;
+                int liczbaPrzejsc = (int)k;
+                Console.WriteLine($"Liczba przejść przez punkty jest równa: {liczbaPrzejsc}");
+                
                 Random randomn = new Random();
-                for (double i = 0; i<=n; i++)
+                Console.WriteLine($"Współrzędne na mapce: ");
+                for (double i = 0; i<=n-1; i++)
                 {
-                    double randomValue = randomk.NextDouble();
-                    listaWygenerowanychPkt.Add(randomValue);
-                    Console.WriteLine($"Współrzędne na mapce: ");
-                    for (double j=0;j<randomValue;j++)
-                    {
-                        Console.WriteLine(randomValue);
-                    }
+                    double randomSzerokosc = Math.Round(randomn.NextDouble() * 50, 2); // losowanie szerokości geograficznej
+                    double randomDlugosc = Math.Round(randomn.NextDouble() * 50, 2); // losowanie długości geograficznej
+                    listaWygenerowanychPkt.Add(randomSzerokosc);
+                    listaWygenerowanychPkt.Add(randomDlugosc);
                 }
+
+                for (int j = 0; j < listaWygenerowanychPkt.Count; j += 2) //wyświetlanie listy
+                {
+                    Console.WriteLine($"Punkt {j / 2 + 1}: Szerokość: {listaWygenerowanychPkt[j]}, Długość: {listaWygenerowanychPkt[j + 1]}");
+                }
+                return 0; ;
             }
-            
 
-            
-            
+            public double PktPoczatekIKoniec(double poczatek, double koniec)
+            {
+                Random random = new Random();
+
+                int losowyIndeksPoczatek = random.Next(listaWygenerowanychPkt.Count / 2) * 2; // uwzględnienie skoku o 2
+                double poczatekSzerokosc = listaWygenerowanychPkt[losowyIndeksPoczatek];
+                double poczatekDlugosc = listaWygenerowanychPkt[losowyIndeksPoczatek + 1];
+
+                int losowyIndeksKoniec = random.Next(listaWygenerowanychPkt.Count / 2) * 2; // uwzględnienie skoku o 2
+                double koniecSzerokosc = listaWygenerowanychPkt[losowyIndeksKoniec];
+                double koniecDlugosc = listaWygenerowanychPkt[losowyIndeksKoniec + 1];
+
+                Console.WriteLine($"Wylosowany punkt początkowy: Szerokość: {poczatekSzerokosc}, Długość: {poczatekDlugosc}");
+                Console.WriteLine($"Wylosowany punkt końcowy: Szerokość: {koniecSzerokosc}, Długość: {koniecDlugosc}");
+                
+                return 0;
+            }
         }
-
-    }
-
-    
+    }  
 }
