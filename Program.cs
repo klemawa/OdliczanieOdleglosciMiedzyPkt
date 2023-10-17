c8ecb18a6499b8aaf6dbf9f67aba4de2c98435cf
klemawa <116188369+klemawa@users.noreply.github.com>
2023-10-17 16:28:03 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdliczanieOdleglosciMiedzyPkt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public class randomPkt
+        {
+            double k;
+            double n = 10;
+            double szerokoscGeo;
+            double dlugoscGeo;
+            List<double> listaWygenerowanychPkt = new List<double>();
+
+            public RandomGeneration(double n,double k) 
+            {
+                Random randomk = new Random();
+                k = randomk.NextDouble();
+                Console.WriteLine($"Liczba przejść przez punkty jest równa: {k}");
+
+                Random randomn = new Random();
+                for (double i = 0; i<=n; i++)
+                {
+                    double randomValue = randomk.NextDouble();
+                    listaWygenerowanychPkt.Add(randomValue);
+                    Console.WriteLine($"Współrzędne na mapce: ");
+                    for (double j=0;j<randomValue;j++)
+                    {
+                        Console.WriteLine(randomValue);
+                    }
+                }
+            }
+            
+
+            
+            
+        }
+
+    }
+
+    
+}
